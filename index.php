b3c55e1f0e96a53c4d84c68fff97f37e546d6f27
Anthony Flowers <id7604ug@gmail.com>
2019-02-17 12:39:18 -0600

    Fixed a single line in the index file that broke the entire page

--- index.php
+++ index.php
@@ -10,38 +10,39 @@
 <?php include('header.php'); ?>
 
 <!-- Page Start -->
-
-    <h1>Final Project 1</h1>
-    <div id="tableView">
-      <table calss="table table-striped" id="agileTeamsTable">
-        <div class="table responsive">
-          <thead>
-            <tr>
-              <th>Team Number</th>
-              <th>Team Name</th>
-              <th>Team Parent Name</th>
-              <th>Team Type</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-              if ($agileTeamsResults->num_rows > 0){
-                while($row = $agileTeamsResults->fetch_assoc()){
-                  echo '<tr>
-                          <td>'.$row['team_id'].'</td>
-                          <td>'.$row["team_name"].'</td>
-                          <td>'.$row["parent_name"].'</td>
-                          <td>'.$row["type"].'</td>
-                        <tr>';
+    <div calss="container-fluid">
+      <h1 class="my-4">Final Project 1</h1>
+      <div id="tableView">
+        <table calss="table table-striped" id="agileTeamsTable">
+          <div class="table responsive">
+            <thead>
+              <tr>
+                <th>Team Number</th>
+                <th>Team Name</th>
+                <th>Team Parent Name</th>
+                <th>Team Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+                if ($agileTeamsResults->num_rows > 0){
+                  while($row = $agileTeamsResults->fetch_assoc()){
+                    echo '<tr>
+                            <td>'.$row['team_id'].'</td>
+                            <td>'.$row["team_name"].'</td>
+                            <td>'.$row["parent_name"].'</td>
+                            <td>'.$row["type"].'</td>
+                          </tr>';
+                  }
+                } else {
+                  echo "0 results";
                 }
-              } else {
-                echo "0 results";
-              }
-            ?>
+              ?>
 
-          </tbody>
-        </div>
-      </table>
+            </tbody>
+          </div>
+        </table>
+      </div>
     </div>
 
 <!-- Page End -- >
