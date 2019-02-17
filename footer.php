a2499345f571b56717dad7a95c5cb6c4a6a21ea6
Anthony Flowers <id7604ug@gmail.com>
2019-02-17 11:10:41 -0600

    Added db integration with datatables

--- footer.php
+++ footer.php
@@ -1,4 +1,43 @@
 
+<!--jQuery-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!--Data Table-->
+<script type="text/javascript" charset="utf8"
+    src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf8"
+    src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" charset="utf8"
+    src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.flash.min.js"></script>
+<script type="text/javascript" charset="utf8"
+    src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+<script type="text/javascript" charset="utf8"
+    src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" charset="utf8"
+    src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.flash.min.js"></script>
+<script type="text/javascript" charset="utf8"
+    src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script type="text/javascript" charset="utf8"
+    src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+
+
+<script type="text/javascript" language="javascript">
+$(document).ready( function () {
+    $('#tableResults').DataTable( {
+        dom: 'Bfrtip',
+            buttons: [
+                'copy', 'excel', 'csv' , 'pdf'
+            ] }
+    );
+
+    $('#ceremoniesTable').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'excel', 'csv', 'pdf'
+        ] }
+    );
+} );
+</script>
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+  require_once("initialize.php");
+
+  $query = "SELECT * FROM trains_and_teams";
+  $GLOBALS['tableResults'] = mysqli_query($db, $query);
 
 ?>
 
@@ -6,8 +10,38 @@
 
 <!-- Page Start -->
 
-    <h1>This is a Heading</h1>
-    <p>This is a paragraph.</p>
+    <h1>Final Project 1</h1>
+    <div id="tableView">
+      <table calss="table table-striped" id="agileTeamsTable">
+        <div class="table responsive">
+          <thead>
+            <tr>
+              <th>Team Number</th>
+              <th>Team Name</th>
+              <th>Team Parent Name</th>
+              <th>Team Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              if ($tableResults->num_rows > 0){
+                while($row = $tableResults->fetch_assoc()){
+                  echo '<tr>
+                          <td>'.$row['team_id'].'</td>
+                          <td>'.$row["team_name"].'</td>
+                          <td>'.$row["parent_name"].'</td>
+                          <td>'.$row["type"].'</td>
+                        <tr>';
+                }
+              } else {
+                echo "0 results";
+              }
+            ?>
+
+          </tbody>
+        </div>
+      </table>
+    </div>
 
 <!-- Page End -- >
 
