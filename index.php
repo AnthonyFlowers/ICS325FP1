e552ccd09a32c2c3a966758a54f5ddd69b4c2a89
Anthony Flowers <id7604ug@gmail.com>
2019-02-17 10:46:11 -0600

    Added php files and test data

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<?php include('header.php'); ?>
+
+<!-- Page Start -->
+
+    <h1>This is a Heading</h1>
+    <p>This is a paragraph.</p>
+
+<!-- Page End -- >
+
+<?php include('footer.php'); ?>
